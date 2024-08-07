fed028f8a6a8df44dd1b31f66c43da824637664e
fawzanoachira <fawzanoachira@gmail.com>
2024-08-07 22:15:36 +0530

    comparison and loops

---
+++ basics/JavaScript/comparison_operators.js
@@ -0,0 +1,7 @@
+let a=10;
+let b="10";
+let c="10";
+console.log(a==b);
+console.log(a==b);
+console.log(c!==b);
+console.log(c===b);
---
+++ basics/JavaScript/loops.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 10;
+let c = 10;
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+
+
+while (b != 0) {
+    console.log("hello");
+    b -= 1;
+}
+
+do {
+    console.log("hiiiiiiii");
+} while (c == 0);
