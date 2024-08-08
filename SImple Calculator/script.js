0d66aca8c5cd2e650d318fdae7912112f16cd574
fawzanoachira <fawzanoachira@gmail.com>
2024-08-08 15:55:34 +0530

    simple calculator

--- SImple Calculator/script.js
+++ SImple Calculator/script.js
@@ -1,13 +1,13 @@
-function clickbutton(val){
-    document.getElementById("screen").value+=val
+function clickbutton(val) {
+    document.getElementById("screen").value += val
 }
 
-function cleardisplay(){
-    document.getElementById("screen").value=""
+function cleardisplay() {
+    document.getElementById("screen").value = ""
 }
 
-function equalcalc(){
-    var ttt=document.getElementById("screen").value
-    var result=eval(ttt)
-    document.getElementById("screen").value=result
+function equalcalc() {
+    var ttt = document.getElementById("screen").value
+    var result = eval(ttt)
+    document.getElementById("screen").value = result
 }
