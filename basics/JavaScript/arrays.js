70204fa2dab92e61114574976b5e37ad3434ef11
fawzanoachira <fawzanoachira@gmail.com>
2024-08-09 16:53:33 +0530

    functions in arrays

---
+++ basics/JavaScript/arrays.js
@@ -0,0 +1,33 @@
+let a = [1, 2, 3, 4, 5];
+
+console.log(a);
+
+console.log(a.length);
+console.log(a.toString());
+console.log(a.at(1));
+console.log(a.join("*"));
+console.log(a.concat(6, 7, 8, 9));
+console.log(a);
+console.log(a.pop());
+console.log(a);
+console.log(a.push(6));
+console.log(a);
+console.log(a.shift());
+console.log(a);
+
+let b = ["ban", "hsf", "fdhas", "jhsd"];
+
+console.log(b.copyWithin(0, 2, 4));
+
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+console.log(arr2.flat(2)); // Output: [1, 2, 3, 4, 5, 6]
+
+const arr3 = [1, 2, [3, 4, [5, 6]]];
+console.log(arr3.flat(1)); // Output: [1, 2, 3, 4, [5, 6]]
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits);
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
+let removed = fruits.splice(2, 2, "Lemon", "Kiwi");
+console.log(removed);
